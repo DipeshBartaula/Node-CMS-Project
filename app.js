ab69f2e3fce485a9a36c3fee97dca8b79ca7a183
DipesBartaula <dipeshbartaula4@gmail.com>
2023-09-06 19:24:40 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+
+//telling the nodejs to set view egine to ejs
+app.set("view engine", "ejs");
+
+// form bata data aairaxa parse gara or handle gar vaneko ho
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// allBlog
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+//createBlog
+app.get("/createBlog", (req, res) => {
+  res.render("createBlog");
+});
+
+//createBlog Post
+app.post("/createBlog", (req, res) => {
+  console.log(req.body.description);
+  res.send("form submitted sucessfully");
+});
+
+app.listen(3000, () => {
+  console.log("Nodejs project has started at 3000 port");
+});

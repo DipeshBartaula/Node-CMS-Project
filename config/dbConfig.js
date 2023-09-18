3040060f39d8d7b9dc408257560dc96174d1099a
DipesBartaula <dipeshbartaula4@gmail.com>
2023-09-18 13:43:07 +0545

    configure db for deployment

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,8 +1,23 @@
+// module.exports = {
+//   HOST: "localhost",
+//   USER: "root",
+//   PASSWORD: "",
+//   DB: "sepcms",
+//   dialect: "mysql",
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     acquire: 30000,
+//     idle: 10000,
+//   },
+// };
+
+//For deployment
 module.exports = {
-  HOST: "localhost",
+  HOST: "containers-us-west-134.railway.app",
   USER: "root",
-  PASSWORD: "",
-  DB: "sepcms",
+  PASSWORD: "dRMTxCubwlmvldD6dH7K",
+  DB: "railway",
   dialect: "mysql",
   pool: {
     max: 5,

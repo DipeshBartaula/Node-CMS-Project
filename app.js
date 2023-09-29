49171d3b93e332987229995238153fa6917214ee
DipesBartaula <dipeshbartaula4@gmail.com>
2023-09-29 22:39:20 +0545

    add gitignore

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const {
   renderEditBlog,
 } = require("./controller/blog/blogController");
 const app = express();
-require('dotenv').config() // requiring dotenv and initiliaixing ti with default configuration
+require("dotenv").config(); // requiring dotenv and initiliaixing ti with default configuration
 
 // database connection
 require("./model/index");
@@ -27,7 +27,7 @@ app.use(express.urlencoded({ extended: true }));
 
 //Routes
 const blogRoute = require("./routes/blogRoute");
-const auth Route = require("./routes/authRoute");
+const authRoute = require("./routes/authRoute");
 
 app.use("", blogRoute); // localhost:3000 + /createBlog === localhost:3000/createBlog
 // app.use("/hello", blogRoute); // localhost:3000/hello +/createBlog = locahost:3000/hello/createBlog
@@ -36,3 +36,6 @@ app.use("", authRoute);
 app.listen(3000, () => {
   console.log("Nodejs project has started at 3000 port");
 });
+
+// To clear git cache
+// git rm -r --cached node_modules
